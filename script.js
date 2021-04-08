3fb34ea28fcd8f05cff3e2f46efc4505e7688048
Kushagra005 <f20200690@pilani.bits-pilani.ac.in>
2021-04-08 13:14:07 +0530

    fixed a bug in hour hand

--- script.js
+++ script.js
@@ -9,5 +9,5 @@ setInterval(() => {
     const hours = date.getHours() % 12;
   second.style.transform = `rotate(${seconds * 6 - 90}deg)`;
   minute.style.transform = `rotate(${minutes * 6 - 90}deg)`;
-  hour.style.transform = `rotate(${hour * 30 - 90}deg)`;
+  hour.style.transform = `rotate(${hours * 30 - 90}deg)`;
 }, 1000);
